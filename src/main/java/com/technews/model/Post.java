d22907e87b34d0e5b55a1c04864a64c38c004f8d
isa <5445946+icortes@users.noreply.github.com>
2022-01-05 14:20:14 -0800

    created Post model and Post class variables

---
+++ src/main/java/com/technews/model/Post.java
@@ -0,0 +1,25 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String postUrl;
+    @Transient
+    private String userName;
+    @Transient
+    private int voteCount;
+    private Integer userId;
+    private Date postedAt = new Date();
+    private Date updatedAt = new Date();
+    private List<Comment> comments;
+}

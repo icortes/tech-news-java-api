b1c97ec78a764a7fe0f075e09a662dcc0f92ce30
isa <5445946+icortes@users.noreply.github.com>
2022-01-05 14:41:06 -0800

    created Comment class and variables

---
+++ src/main/java/com/technews/model/Comment.java
@@ -0,0 +1,17 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String commentText;
+    private Integer userId;
+    private Integer postId;
+}
